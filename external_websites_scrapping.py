ce59451e527634b9f37e562a637c81102d3f7f62
PulkitGargShorthillsAI <pulkit.garg@shorthills.ai>
2025-03-05 11:26:54 +0530

    added the extracted content in a new file

--- external_websites_scrapping.py
+++ external_websites_scrapping.py
@@ -7,6 +7,8 @@ def scrapping():
 
     soup = BeautifulSoup(html_text,'lxml')
     jobs = soup.find_all('div',class_="srp-listing clearfix")
+
+    f = open('job_openings.txt','w')
     for job in jobs:
         # print(job.prettify())
         company_name = job.find('h4').find('span',class_="srp-comp-name").text
@@ -14,13 +16,17 @@ def scrapping():
         skills = job.find_all('a',class_="srphglt")
 
         print('Company name :',company_name)
+        f.write(f'Company name : {company_name}\n')
         skill_arr = ""
         
         for skill in skills:
             skill_arr += skill.text + ','
-        print("Skills : ",skill_arr)
+        print(f"Skills : {skill_arr}")
         print(f"For more info : {info}")
         print(f'\n\n\n')
-
+        f.write(f"Skills : {skill_arr}\n")
+        f.write(f"For more info : {info}\n")
+        f.write(f'\n\n\n')
+    f.close()
 
 scrapping()
